23359aa49e2535647ad8aa3d6169a4063f819854
James Reid <jreidmke@gmail.com>
2021-07-04 13:41:48 -0500

    done for the day

--- src/components/New.js
+++ src/components/New.js
@@ -1,9 +1,40 @@
+import { useState } from "react";
+
 function New() {
+    const [formData, setFormData] = useState({
+        title: "",
+        content: ""
+    });
+
+    function handleChange(e) {
+        const { name, value }= e.target;
+        setFormData(data => ({
+            ...data,
+            [name]: value
+        }));
+    };
+
     return(
         <div>
-            <h1>New</h1>
+            <form>
+                <input
+                    name="title"
+                    value={formData.title}
+                    type="text"
+                    onChange={handleChange}
+                    placeholder="TITLE"
+                />
+                <input
+                    name="content"
+                    value={formData.content}
+                    type="text"
+                    onChange={handleChange}
+                    placeholder="CONTENT"
+                />
+            <button>Submit</button>
+            </form>
         </div>
     )
-}
+};
 
 export default New;
